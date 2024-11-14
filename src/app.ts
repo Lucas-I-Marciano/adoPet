5ed8e6df3a1248c4d6284adf029512eba00e1021
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-14 16:47:39 -0300

    :sparkles: feat: installing typeORM

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express";
-// import { petsRouter } from "./routes/petsRoute.js";
 import router from "./routes/index.js";
+import "reflect-metadata";
+import { AppDataSource } from "./config/data-source.js";
 
 const app = express();
 app.use(express.json());
 router(app);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database initialized");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to TypeScript course!");
