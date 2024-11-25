c8ae55126049239d974f9e8745e1c7694ddf8ff3
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-25 13:41:47 -0300

    :sparkles: feat: list of Adopters devised
    Beside creation of adopters list, empliciting the return of each function

--- src/repositories/AdopterRepository.ts
+++ src/repositories/AdopterRepository.ts
@@ -5,12 +5,15 @@ import { AdopterEntity } from "../entities/AdopterEntity.js";
 const adopterRepository = AppDataSource.getRepository(AdopterEntity);
 
 export class AdopterRepository {
-  createAdopter(adopter: AdopterEntity) {
+  createAdopter(adopter: AdopterEntity): void {
     adopterRepository.save(adopter);
   }
-  findAdopterById(id: number) {
+  findAdopterById(id: number): Promise<AdopterEntity | null> {
     return adopterRepository.findOneBy({
       id: id,
     });
+  }
+  listAdopters(): Promise<AdopterEntity[]> {
+    return adopterRepository.find();
   }
 }
