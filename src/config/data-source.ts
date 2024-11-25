1f4761aac3b54538ca0e8e98e6dc89defc86d80f
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-25 13:15:00 -0300

    :sparkles: feat: Adding Address Entity on DataSource

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm";
 import { PetEntity } from "../entities/PetEntity.js";
 import { AdopterEntity } from "../entities/AdopterEntity.js";
+import { AddressEntity } from "../entities/AddressEntity.js";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/config/database.sqlite",
-  entities: [PetEntity, AdopterEntity],
+  entities: [PetEntity, AdopterEntity, AddressEntity],
   synchronize: true,
 });
