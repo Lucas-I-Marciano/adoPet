75b0efa8534be496d1a0457c972bf4bd0eba3f64
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-25 17:04:11 -0300

    :sparkles: feat: Creating 'adoPet' function to tracking adoptions

--- src/repositories/PetRepository.ts
+++ src/repositories/PetRepository.ts
@@ -1,8 +1,10 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../config/data-source.js";
 import { PetEntity } from "../entities/PetEntity.js";
+import { AdopterEntity } from "../entities/AdopterEntity.js";
 
 export const petRepository = AppDataSource.getRepository(PetEntity);
+const adopterRepository = AppDataSource.getRepository(AdopterEntity);
 
 export class PetRepository {
   async createPet(pet: PetEntity): Promise<PetEntity> {
@@ -30,5 +32,19 @@ export class PetRepository {
     const pet = await petRepository.findOneBy({ id });
     const treatedPet = pet ? pet : <PetEntity>{};
     await petRepository.remove(treatedPet);
+  }
+
+  async adoPet(petId: number, adopterId: number) {
+    const pet = await petRepository.findOneBy({ id: petId });
+    const adopter = await adopterRepository.findOneBy({ id: adopterId });
+    if (!pet) {
+      throw new Error("Pet not founded");
+    }
+    if (!adopter) {
+      throw new Error("Adopter not founded");
+    }
+    pet.adopter = adopter;
+    pet.adopted = true;
+    await petRepository.save(pet);
   }
 }
